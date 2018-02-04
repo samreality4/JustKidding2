2ac0c16d4b78198e1e999cf16b952453f3499d45
samreality4 <samreality4@gmail.com>
2018-02-04 09:20:08 -0800

    commit from windoow

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/TestforNonString.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TestforNonString.java
@@ -1,14 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ApplicationTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+
+import com.udacity.gradle.builtitbigger.paid.MainActivity;
+import com.udacity.gradle.builtitbigger.paid.MainActivityFragment;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -23,24 +30,25 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.mockito.Mockito.mock;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class TestforNonString {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-
-    @Rule
-    public IntentsTestRule<MainActivity> intentTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
-    public void testforNonString() {
-        onView(withId(R.id.instructions_text_view)).perform(click());
+    public void AsyncTaskTest(){
+        String result = mock(EndpointAsyncTask.class);
+
+
+
 
 
     }
+
+
+
 
 
 }
